767e16925b84da986a7222155be8fb0b51a27249
Spencer Cheng <spencer.cheng@holbertonschool.com>
2017-03-21 00:42:50 +0000

    Added comments to functions

--- testdir/willisfunctions.c
+++ testdir/willisfunctions.c
@@ -2,10 +2,15 @@
 #include "holberton.h"
 #include <stdarg.h>
 
+/**
+ * _putchar - puts char to std output
+ * @args: arguements
+ * Return: int
+ */
 int _putchar(va_list args)
 {
 	char c;
-	
+
 	c = va_arg(args, int);
 
 	(write(1, &c, 1));
@@ -14,6 +19,11 @@ int _putchar(va_list args)
 }
 
 
+/**
+ * print_string - prints string to std output
+ * @args: arguements
+ * Return: int
+ */
 int print_string(va_list args)
 {
 	int i;
@@ -21,17 +31,22 @@ int print_string(va_list args)
 
 	s = va_arg(args, char *);
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *s != '\0'; i++)
 	{
 		(write(1, s, 1));
 		s++;
-	}
-	
+	};
+
 	return (i);
 }
 
+/**
+ * print_unsigned - puts unsigned int to std output
+ * @b: arguements
+ * Return: int
+ */
 int print_unsigned(unsigned int b)
-{	
+{
 	static int x;
 	int y;
 
@@ -40,17 +55,22 @@ int print_unsigned(unsigned int b)
 	if (b / 10 != 0)
 	{
 		x++;
-		print_unsigned(b / 10);
+		print_unsigned (b / 10);
 	}
 
 	y = ((b % 10) + '0');
-	
+
 	(write(1, &y, 1));
 
 	return (y);
 }
 
 
+/**
+ * print_integer - puts integer to std output
+ * @args: arguements
+ * Return: int
+ */
 int print_integer(va_list args)
 {
 	char g;
@@ -61,19 +81,24 @@ int print_integer(va_list args)
 	j = 0;
 
 	g = '-';
-		
+
 	if (i < 0)
 	{
 		write(1, &g, 1);
 		i = -i;
 		j++;
 	}
-	
+
 	j += print_unsigned((unsigned int) i);
 
 	return (j);
 }
 
+/**
+ * print_char - puts char to std output
+ * @c: char argument
+ * Return: int
+ */
 int print_char(char c)
 {
 	write(1, &c, 1);
